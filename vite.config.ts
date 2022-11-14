6fe8ce0521678043c95ca388609c26b57a19cce5
guilherme augusto <guilherme.silva@ftdata.com>
2022-11-14 03:25:00 -0300

    ajustando url da versel

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/portifolio/dist/",
+  base: "https://portifolio-kt81rfu5h-guilhermeaugustosfc.vercel.app/",
 });
