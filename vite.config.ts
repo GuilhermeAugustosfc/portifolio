a9adc69f6b40887e15ecbdc5178282dfef8984d6
guilherme augusto <guilherme.silva@ftdata.com>
2022-11-14 03:34:33 -0300

    fix

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,4 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "https://portifolio-kt81rfu5h-guilhermeaugustosfc.vercel.app/",
 });
