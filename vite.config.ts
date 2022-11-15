f22fb7eb80fd266ad2974aec1c2c1965a9ed3b9d
guilherme augusto <guilherme.silva@ftdata.com>
2022-11-14 22:42:12 -0300

    base url build

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "https://portifolio-f0muk34pw-guilhermeaugustosfc.vercel.app/",
+  base: "./",
 });
